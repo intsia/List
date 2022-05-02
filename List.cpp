926fd98f36cd21c19740624a2d433f7045375349
Никита Платонов <nplatonov.private@gmail.com>
2022-05-02 12:19:45 +0300

    Добавьте файлы проекта.

---
+++ List.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <assert.h>
+ 
+struct Node {
+    int val_;
+    Node* next_;
+};
+ 
+struct List {
+    size_t size_;
+    Node* head_;
+};
+ 
+void Insert(List* list, Node* pos, int val) {
+    Node* node = (Node*) malloc(sizeof(Node));
+    node->val_ = val;
+    pos->next_ = node;
+    list->size_++;
+}
+
+void Remove()
+ 
+int main(const int argc, const char* argv[]) {
+    List l;
+    return 0;
+}
+ 
